d3265cbc4c986c729e567ac72e1173dfcd3f8451
Caihuanyu <eterlf41@gmail.com>
2020-03-30 11:52:34 +0800

    initial

---
+++ src/app.config.js
@@ -0,0 +1,25 @@
+const pages = ['pages/index/index'];
+
+exports.alipay = {
+  pages,
+  window: {
+    defaultTitle: 'Remax Alipay Template',
+    titleBarColor: '#282c34'
+  }
+};
+
+exports.wechat = {
+  pages,
+  window: {
+    navigationBarTitleText: 'Remax Wechat Template',
+    navigationBarBackgroundColor: '#282c34'
+  }
+};
+
+exports.toutiao = {
+  pages,
+  window: {
+    navigationBarTitleText: 'Remax Toutiao Template',
+    navigationBarBackgroundColor: '#282c34'
+  }
+};
